f4c1f98d75b40db9bc7ba9a66b9e01f669300ef3
Unknown <kho11@yandex.ru>
2017-10-13 08:51:15 +0500

    Trade history Tests

--- BinanceNet.Tests/BinanceClient/TradeHistoryTests.cs
+++ BinanceNet.Tests/BinanceClient/TradeHistoryTests.cs
@@ -9,60 +9,67 @@ using BinanceNet.Tests.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BinanceNet.Tests.BinanceClient {
+
     [TestClass]
-    public class DepthBookTests {
+    public class TradeHistoryTests {
         public static IBinanceClient Client { get; set; }
-        public static IDepthBook Book { get; set; }
+        public static ITradeHistory Book { get; set; }
         public static string Symbol { get; set; }
 
         [ClassInitialize]
         public static async void ClassInit(TestContext context) {
-            Symbol = TestsConfig.GetPairToTest();
+            Symbol = TestsConfig.GetPairToTest ();
             Client = new BinanceNet.BinanceClient ();
-            Book = await Client.GetDepthBookAsync(Symbol);
+            Book = await Client.GetTradeHistoryAsync(Symbol);
         }
 
         [TestMethod]
         public void IsBookGetted() {
-            Assert.IsNotNull(Book);
-            Assert.AreEqual(Symbol, Book.Symbol);
+            Assert.IsNotNull (Book);
+            Assert.AreEqual (Symbol, Book.Symbol);
         }
 
         [TestMethod]
         public void IsDataInBook() {
-            Assert.IsTrue (Book.Bids.Length > 0);
-            Assert.IsTrue (Book.Asks.Length > 0);
+            Assert.IsTrue(Book.CurrentData.Trades.Length > 0);
         }
 
         [TestMethod]
-        public void IsFirstBidLowerThanFirstAsk() {
-            Assert.IsTrue(Book.Bids.First().Price < Book.Asks.First().Price);
+        public void IsFirstAggIdLowerThanLast() {
+            Assert.IsTrue(Book.CurrentData.FirstAggId < Book.CurrentData.LastAggId);
         }
+
+        [TestMethod]
+        public void IsFirstTradeIdLowerThanLast() {
+            Assert.IsTrue (Book.CurrentData.FirstTradeId < Book.CurrentData.LastTradeId);
+        }
+
 
         [TestMethod]
         public void IsRefreshWorksCorrectly() {
             var lastUpdateTime = Book.LastUpdateTime;
-            Book.RefreshData();
-            Assert.AreNotEqual(lastUpdateTime, Book.LastUpdateTime);
+            var data = Book.CurrentData;
+            Book.RefreshData ();
+            Assert.AreNotEqual (lastUpdateTime, Book.LastUpdateTime);
         }
 
         [TestMethod]
         public void IsListeningWorks() {
             // Book should not listen before this test
-            Assert.IsFalse(Book.IsListening);
+            Assert.IsFalse (Book.IsListening);
             var startTime = Book.LastUpdateTime;
 
             int updatesCount = 0;
             // Subscribe on update to increase updatesCount
             Book.OnDepthUpdate += (sender, data) => {
                 updatesCount++;
-                Assert.IsNotNull(data);
+                Assert.IsNotNull (data);
             };
             // If not any events happen in 10 sec than test is failed
-            Thread.Sleep(10000);
+            Thread.Sleep (10000);
 
-            Assert.IsTrue(Book.LastUpdateTime != startTime);
-            Assert.IsTrue(updatesCount > 0);
+            Assert.IsTrue (Book.LastUpdateTime != startTime);
+            Assert.IsTrue (updatesCount > 0);
         }
     }
 }
