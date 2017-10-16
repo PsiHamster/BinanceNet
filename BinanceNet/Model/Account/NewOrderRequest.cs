da0bd10ee67c7e2285b751a7f28d8fb30d70743e
Unknown <kho11@yandex.ru>
2017-10-16 12:56:49 +0500

    IAccount now contains orders

--- BinanceNet/Model/Account/NewOrderRequest.cs
+++ BinanceNet/Model/Account/NewOrderRequest.cs
@@ -9,27 +9,49 @@ namespace BinanceNet.Model.Account {
 
     [JsonObject (MemberSerialization.OptIn)]
     public struct NewOrderRequest {
+
         [JsonProperty ("symbol"), JsonRequired]
         public string Symbol { get; set; }
+
         [JsonProperty ("side"), JsonRequired]
         [JsonConverter(typeof(StringEnumConverter))]
         public OrderSide OrderSide { get; set; }
+
         [JsonProperty ("type"), JsonRequired]
         [JsonConverter(typeof(StringEnumConverter))]
         public OrderType Type { get; set; }
+
         [JsonProperty ("timeInForce"), JsonRequired]
         [JsonConverter(typeof(StringEnumConverter))]
         public TimeInForce TimeInForce { get; set; }
+
         [JsonProperty ("quantity"), JsonRequired]
         public decimal Quantity { get; set; }
+
         [JsonProperty ("price"), JsonRequired]
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// A unique id for the order. Automatically generated if not sent.
+        /// Not mandatory
+        /// </summary>
         [JsonProperty ("newClientOrderId")]
         public string NewClientOrderId { get; set; }
+
+        /// <summary>
+        /// Used with stop orders
+        /// Not mandatory
+        /// </summary>
         [JsonProperty ("stopPrice")]
         public decimal? StopPrice { get; set; }
+
+        /// <summary>
+        /// Used with iceberg orders
+        /// Not mandatory
+        /// </summary>
         [JsonProperty ("icebergOnly")]
         public decimal? IcebergOnly { get; set; }
+
         [JsonProperty ("timestamp"), JsonRequired]
         public TimeStamp Timestamp { get; set; }
     }
